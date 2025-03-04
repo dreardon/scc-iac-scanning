fafa7453948cbad9ae6b9db899111310a7768027
Avnit Bambah <abambah+github@gmail.com>
2025-03-04 15:39:34 -0800

    sql changes

--- iac_eval.sh
+++ iac_eval.sh
@@ -2,8 +2,8 @@
 export _ORGANIZATION_ID=[ORG_ID]
 export TF_VAR_PROJECT_ID=[PROJECT_ID]
 export TF_VAR_BQ_OWNER=[EMAIL e.g. test@test.com]
-export TF_VAR_ZONE=[ZONE e.g. us-central1-a]
-export TF_VAR_REGION=[REGION e.g. us-central1] 
+export TF_VAR_ZONE=us-central1-a
+export TF_VAR_REGION=us-central1
 
 terraform plan -out mainplan.tfplan
 
