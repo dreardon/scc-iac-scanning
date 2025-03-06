57ff2c66268d7b6c148e805a2aaa1a1cff431ed1
Daniel Reardon <danreardon@gmail.com>
2025-03-06 21:47:04 +0000

    Add bigquery checks

--- bigquery.tf
+++ bigquery.tf
@@ -3,15 +3,55 @@ resource "google_bigquery_dataset" "public" {
   project       = var.PROJECT_ID
   friendly_name = "test"
   description   = "This dataset is public"
-  # location                    = "EU"
   default_table_expiration_ms = 3600000
 
-  access {
-    role       = "READER"
-    iam_member = "allUsers"
+  default_encryption_configuration {
+    kms_key_name = ""
   }
+
   access {
     role          = "OWNER"
     user_by_email = var.BQ_OWNER
   }
+
+  access {
+    role          = "READER"
+    special_group = "allAuthenticatedUsers"
+  }
+
+  access {
+    role = "READER"
+    iam_member = "allUsers"
+  }
+
+}
+
+resource "google_bigquery_table" "default" {
+  dataset_id = google_bigquery_dataset.public.dataset_id
+  table_id   = "bar"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "permalink",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "The Permalink"
+  },
+  {
+    "name": "state",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "State where the head office is located"
+  }
+]
+EOF
 }
