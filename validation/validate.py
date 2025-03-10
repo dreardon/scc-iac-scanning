4d25be812d5882fbd0ada15b93393206f5118f04
Daniel Reardon <danreardon@gmail.com>
2025-03-10 19:43:56 +0000

    Add counts

--- validation/validate.py
+++ validation/validate.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import ast
 
 found_detectors = sys.argv[1].replace('\n',' ').split(' ')
-print('Found detectors:',found_detectors)
+print('Found detectors:',len(found_detectors),found_detectors)
 
 url = "https://cloud.google.com/security-command-center/docs/supported-iac-assets-policies#security-health-analytics-built-in-detectors"
 response = requests.get(url)
@@ -19,8 +19,8 @@ if sha_tag:
         for code_tag in ul_tag.find_all("code", translate="no", dir="ltr"):
             supported_detectors.append(code_tag.text.strip())
 
-print('Supported detectors:',supported_detectors)
+print('Supported detectors:',len(supported_detectors),supported_detectors)
 
 
 missing_detectors = list(set(supported_detectors) - set(found_detectors))
-print('Missing detectors:',missing_detectors)
+print('Missing detectors:',len(missing_detectors),missing_detectors)
